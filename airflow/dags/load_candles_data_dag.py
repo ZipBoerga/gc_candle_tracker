f762a46111bfcf9831dfb810f2a96acbe1e22db8
ZipBoerga <zip_boerga@tutamail.com>
2024-07-20 17:12:57 +0300

    tested ingest, connections for aiflow, better airflow confs in docker-compose

--- airflow/dags/load_candles_data_dag.py
+++ airflow/dags/load_candles_data_dag.py
@@ -9,7 +9,7 @@ import utils.scrapping as scrap
 import utils.queries as queries
 
 
-@dag(schedule='0 13 * * *', start_date=datetime(2023, 7, 19), catchup=True)
+@dag(schedule='0 13 * * *', start_date=datetime(2023, 7, 19), catchup=False)
 def load_candles_data():
     @task()
     def get_urls() -> list[str]:
@@ -19,7 +19,9 @@ def load_candles_data():
     @task()
     def get_candles_data(urls: list[str]) -> list[dict]:
         candles: list[dict] = []
-        for url in urls:
+        for i, url in enumerate(urls):
+            if (i + 1) % 10 == 0 and i != 0:
+                print(f'Fetched {i + 1} urls from {len(urls)}')
             candles.append(scrap.get_candle_details(url))
         return candles
 
@@ -33,7 +35,7 @@ def load_candles_data():
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
 
-        for candle in candles:
+        for i, candle in enumerate(candles):
             id_ = f"{candle['candle_id']}_{candle['processing_date']}"
             cursor.execute(
                 queries.history_query,
@@ -60,10 +62,14 @@ def load_candles_data():
             cursor.execute(price_query, (candle['candle_id'], candle['price']))
             conn.commit()
 
+            if (i + 1) == 0 and i != 0:
+                print(f'Wrote {i + 1} candles to db from {len(candle)}')
+
     get_urls_task = get_urls()
     get_candles_data_task = get_candles_data(get_urls_task)
     write_to_db_task = write_to_db(get_candles_data_task)
 
     get_urls_task >> get_candles_data_task >> write_to_db_task
+
 
 load_candles_data_dag = load_candles_data()
